d837dbffe318b53fcf1ee47fa0e887c1f3a61acd
MauriDemasi <mauridemasi88@gmail.com>
2024-04-17 10:30:35 -0300

    End Section Estrenos

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,55 +12,45 @@ const options = {
 
 // Hacer la solicitud a la API
 fetch(
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=es-AR&page=1&sort_by=popularity.desc",
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=es-AR&page=1&sort_by=popularity.desc",
   options
 )
   .then((response) => response.json())
   .then((data) => {
     // Iterar sobre los resultados de la API
     data.results.forEach((movie) => {
+      const cardColumn = document.createElement("div");
+      cardColumn.classList.add("col-lg-3", "col-md-4", "col-sm-6", "mb-4");
+
       // Crear elemento de tarjeta
       const card = document.createElement("div");
-      card.classList.add(
-        "card",
-        "col-xl-3", // 4 tarjetas en pantallas extra grandes
-        "col-lg-4", // 3 tarjetas en pantallas grandes
-        "col-md-6", // 2 tarjetas en pantallas medianas
-        "col-sm-12", // 1 tarjeta en pantallas pequeñas
-        "w-100", // Asegura que la tarjeta ocupe todo el ancho disponible
-        "booking-card",
-        "v-2",
-        "rounded-bottom"
-      );
+      card.classList.add("card", "booking-card", "v-2", "rounded-bottom");
 
       // Construir contenido de la tarjeta
       card.innerHTML = `
         <div class="bg-image hover-overlay ripple ripple-surface ripple-surface-light" data-mdb-ripple-color="light">
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="img-thumbnail">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="img-thumbnail card-img">
         </div>
         <div class="card-body">
-          <h5 class="card-title font-weight-bold"><a>${movie.title}</a></h5>
-          <ul class="list-unstyled list-inline mb-0">
+          <h4 class="card-title font-weight-bold movie-title"><a>${movie.title}</a></h4>
+          <ul class="list-unstyled list-inline mb-2">
             <li class="list-inline-item me-0">
               <i class="fas fa-star text-warning fa-xs"> </i>
             </li>
             <li class="list-inline-item me-0">
-              <i class="fas fa-star text-warning fa-xs"></i>
+              <i class="fas fa-star text-warning fa-xs"> </i>
             </li>
             <li class="list-inline-item me-0">
-              <i class="fas fa-star text-warning fa-xs"></i>
+              <i class="fas fa-star text-warning fa-xs"> </i>
             </li>
             <li class="list-inline-item me-0">
-              <i class="fas fa-star text-warning fa-xs"></i>
+              <i class="fas fa-star text-warning fa-xs"> </i>
             </li>
-            <li class="list-inline-item">
-              <i class="fas fa-star-half-alt text-warning fa-xs"></i>
-            </li>
-            <li class="list-inline-item">
-              <p class="text-muted">${movie.vote_average} (${movie.vote_count})</p>
+            <li class="list-inline-item me-0">
+              <i class="fas fa-star text-warning fa-xs"> </i>
             </li>
           </ul>
-          <p class="card-text">
+          <p class="card-text overview">
             ${movie.overview}
           </p>
           <hr class="my-4" />
@@ -68,7 +58,8 @@ fetch(
       `;
 
       // Agregar la tarjeta al contenedor de películas
-      movieContainer.appendChild(card);
+      cardColumn.appendChild(card);
+      document.getElementById("movie-container").appendChild(cardColumn);
     });
   })
   .catch((err) => console.error(err));
