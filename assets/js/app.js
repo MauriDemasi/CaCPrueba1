c25d6e326aa4ca1cf3e73575ca51191c715dff5b
MauriDemasi <mauridemasi88@gmail.com>
2024-04-18 18:50:23 -0300

    Dropdown with categories

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,3 @@
-// Lugar donde deseas mostrar las tarjetas de películas
-const movieContainer = document.getElementById("movie-container");
-const MAX_RESULTS = 12; // Número máximo de películas a mostrar
-
 const options = {
   method: "GET",
   headers: {
@@ -11,7 +7,11 @@ const options = {
   },
 };
 
-// Hacer la solicitud a la API
+// Lugar donde deseas mostrar las tarjetas de películas
+const movieContainer = document.getElementById("movie-container");
+const MAX_RESULTS = 12; // Número máximo de películas a mostrar
+
+// Hacer la solicitud a la API para obtener las películas estreno
 fetch(
   "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=es-AR&page=1&sort_by=popularity.desc",
   options
@@ -67,3 +67,69 @@ fetch(
   })
 
   .catch((err) => console.error(err));
+
+//Funcion para hacer los fetch por categorias y renderizar las card en su respectivon contenedor
+function getResultsByGenre(genreId, containerId) {
+  const documentaryContainer = document.getElementById(containerId);
+
+  fetch(
+    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=es-AR&page=1&sort_by=popularity.desc&with_genres=${genreId}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Iterar sobre los resultados de la API
+      data.results.forEach((movie) => {
+        const cardColumn = document.createElement("div");
+        cardColumn.classList.add("col-lg-3", "col-md-4", "col-sm-6", "mb-4");
+
+        if (documentaryContainer.children.length < MAX_RESULTS) {
+          // Crear elemento de tarjeta
+          const card = document.createElement("div");
+          card.classList.add("card", "booking-card", "v-2", "rounded-bottom");
+
+          // Construir contenido de la tarjeta
+          card.innerHTML = `
+            <div class="bg-image hover-overlay ripple ripple-surface ripple-surface-light" data-mdb-ripple-color="light">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="img-thumbnail card-img">
+            </div>
+            <div class="card-body">
+              <h4 class="card-title font-weight-bold movie-title"><a>${movie.title}</a></h4>
+              <ul class="list-unstyled list-inline mb-2">
+                <li class="list-inline-item me-0">
+                  <i class="fas fa-star text-warning fa-xs"> </i>
+                </li>
+                <li class="list-inline-item me-0">
+                  <i class="fas fa-star text-warning fa-xs"> </i>
+                </li>
+                <li class="list-inline-item me-0">
+                  <i class="fas fa-star text-warning fa-xs"> </i>
+                </li>
+                <li class="list-inline-item me-0">
+                  <i class="fas fa-star text-warning fa-xs"> </i>
+                </li>
+                <li class="list-inline-item me-0">
+                  <i class="fas fa-star text-warning fa-xs"> </i>
+                </li>
+              </ul>
+              <p class="card-text overview">
+                ${movie.overview}
+              </p>
+              <hr class="my-4" />
+            </div>
+          `;
+
+          // Agregar la tarjeta al contenedor de películas
+          cardColumn.appendChild(card);
+          documentaryContainer.appendChild(cardColumn);
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching documentaries:", error);
+    });
+}
+//Documentales
+getResultsByGenre(99, "documentary-container");
+//Animadas
+getResultsByGenre(16, "animated-container");
